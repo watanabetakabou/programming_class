1763fdf85557ae96a2be109b0d4b9035e04df826
Takaboh <57132812+watanabetakabou@users.noreply.github.com>
2021-12-15 08:37:01 +0900

    ソ連国家を作ろうとした

---
+++ main.py
@@ -0,0 +1,5 @@
+def on_forever():
+    music.play_melody("G G G G G - - - ", 120)
+    music.play_melody("G C5 C5 G A B  - ", 120)
+    music.play_melody("- - - - - - - - ", 120)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    music.playMelody("G G G G G - - - ", 120)
+    music.playMelody("G C5 C5 G A B  - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
 })
