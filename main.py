6a57dcca8465a82d42c4d103c1202192193792aa
Takaboh <57132812+watanabetakabou@users.noreply.github.com>
2021-12-16 18:33:23 +0900

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def on_forever():
-    music.play_melody("G G G G G - - - ", 120)
-    music.play_melody("G C5 C5 G A B  - ", 120)
-    music.play_melody("- - - - - - - - ", 120)
+    music.play_melody("E E E E F F E F ", 250)
+    music.play_melody("G G B B G G G G ", 250)
+    music.play_melody("F F E F G G G G ", 125)
+    music.play_melody("E5 E5  - - - - - ", 120)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
-basic.forever(function on_forever() {
-    music.playMelody("G G G G G - - - ", 120)
-    music.playMelody("G C5 C5 G A B  - ", 120)
-    music.playMelody("- - - - - - - - ", 120)
+function Shougatsu () {
+    music.setVolume(255)
+    music.playMelody("E E E E F F E F ", 250)
+    music.playMelody("G G B B G G G G ", 250)
+    music.playMelody("F F E F G G G G ", 125)
+    music.rest(music.beat(BeatFraction.Whole))
+}
+basic.forever(function () {
+    Shougatsu()
 })
