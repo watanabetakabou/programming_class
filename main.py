7ee8d1666fcbe44834c6738888dbd8de0438f189
Takaboh <57132812+watanabetakabou@users.noreply.github.com>
2022-12-27 16:52:19 +0900

    RTA追加

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def on_forever():
+def Shougatsu():
+    music.set_volume(255)
     music.play_melody("E E E E F F E F ", 250)
     music.play_melody("G G B B G G G G ", 250)
     music.play_melody("F F E F G G G G ", 125)
-    music.play_melody("E5 E5  - - - - - ", 120)
+    music.rest(music.beat(BeatFraction.WHOLE))
+def FamiMa():
+    music.play_melody("- - - - - - - - ", 120)
+music.start_melody(music.built_in_melody(Melodies.ENTERTAINER),
+    MelodyOptions.FOREVER_IN_BACKGROUND)
+
+def on_forever():
+    if cuteBot.tracking(cuteBot.TrackingState.L_R_LINE):
+        cuteBot.motors(25, 25)
+    elif cuteBot.tracking(cuteBot.TrackingState.L_UNLINE_R_LINE):
+        cuteBot.motors(25, -15)
+    elif cuteBot.tracking(cuteBot.TrackingState.L_LINE_R_UNLINE):
+        cuteBot.motors(-15, 25)
+    else:
+        basic.pause(250)
+        cuteBot.motors(25, -25)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+function RTA () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.motors(velocity, velocity)
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        direction = 1
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, randint(0, 255), randint(0, 255), randint(0, 255))
+        cuteBot.motors(velocity, 0)
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        direction = -1
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, randint(0, 255), randint(0, 255), randint(0, 255))
+        cuteBot.motors(0, velocity)
+    } else {
+        if (direction == -1) {
+            cuteBot.motors(-23, velocity)
+        } else {
+            cuteBot.motors(velocity, -23)
+        }
+    }
+}
 function Shougatsu () {
     music.setVolume(255)
     music.playMelody("E E E E F F E F ", 250)
@@ -5,6 +24,13 @@ function Shougatsu () {
     music.playMelody("F F E F G G G G ", 125)
     music.rest(music.beat(BeatFraction.Whole))
 }
+function FamiMa () {
+    music.playMelody("- - - - - - - - ", 120)
+}
+let velocity = 0
+let direction = 0
+direction = 0
+velocity = 70
 basic.forever(function () {
-    Shougatsu()
+    RTA()
 })
